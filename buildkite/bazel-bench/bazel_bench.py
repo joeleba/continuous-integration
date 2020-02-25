8d7fc36334318c23399d9f3d7634e8fab5e8e4b4
Joe Le-Ba <leba@google.com>
2020-02-25 14:44:04 +0100

    -r

--- buildkite/bazel-bench/bazel_bench.py
+++ buildkite/bazel-bench/bazel_bench.py
@@ -369,7 +369,7 @@ def _report_generation_step(
     Also update the path reserved for the latest report of each project.
     """
     commands = []
-    commands.append("python3 -m pip install third_party/requirements.txt")
+    commands.append("python3 -m pip install -r third_party/requirements.txt")
     # commands.append(" ".join([
     #     "bazel",
     #     "run",
